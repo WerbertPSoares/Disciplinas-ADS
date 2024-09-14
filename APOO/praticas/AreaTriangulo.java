fd16a2f9015ab71efe987e066c010e9c06d2601e
WerbertPSoares <wp.soares0107@gmail.com>
2024-09-13 22:57:07 -0300

    Create AreaTriangulo.java

---
+++ APOO/praticas/AreaTriangulo.java
@@ -0,0 +1,18 @@
+package APOO.praticas;
+public class AreaTriangulo {
+
+    public static void main(String[] args) {
+       // declaração da variáveis
+int comprimento = 4;
+int largura = 6;
+int area;
+
+// Calcula a area do triangulo
+area = comprimento * largura;
+
+// impressão das variaveis
+System.out.println("Se o comprimento é: " + comprimento);
+System.out.println("E a largura é: " + largura);
+System.out.println("Então a area do retangulo é: " + area);
+    }
+}
